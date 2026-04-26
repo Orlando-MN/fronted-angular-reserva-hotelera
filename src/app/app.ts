0bc9fcde715aa1f8d88013296c93447b069bf18b
Orlando <orlandomeza1821@gmail.com>
2026-04-26 14:46:24 -0500

    feat: crear NavBar e integrar RemixIcon v28

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopHeader } from './components/common/top-header/top-header';
+import { Navbar } from './components/common/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TopHeader,
+    Navbar
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
