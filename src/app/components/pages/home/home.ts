229a23cc3eeff52b24e8328fce2836323f656f59
Orlando <orlandomeza1821@gmail.com>
2026-04-26 17:24:22 -0500

    feat: implementar buscador de hoteles y configuración de ng2-date-picker v35

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { DpDatePickerModule } from 'ng2-date-picker';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    DpDatePickerModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  datePickerConfig = {
+    format: 'YYYY-MM-DD'
+  }
+}
